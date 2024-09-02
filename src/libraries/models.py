d3b1ce7ff6a36cf115d54ce9af3d2e6d0b61aecb
Kevin Woodfield <kcwoodfield@gmail.com>
2024-09-01 21:45:12 -0500

    library entry model

---
+++ src/libraries/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+# LibraryEntry model for a Book
+class LibraryEntry(models.Model):
+    title = models.CharField(max_length=265)
+    author = models.CharField(max_length=100)
+    pages = models.IntegerField()
+    rating = models.IntegerField()
+    review = models.TextField()
+    completed = models.BooleanField(default=False)
+    timestamp = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
