f0e858045113d27399078aebaf13889b14de1618
Kevin Woodfield <kcwoodfield@gmail.com>
2024-09-02 07:57:46 -0500

    add Create and Get Library schemas

--- src/libraries/schemas.py
+++ src/libraries/schemas.py
@@ -1,20 +1,23 @@
+from datetime import datetime
 from ninja import Schema
-
-"""
-This module contains the schema definition for creating a library entry.
-
-Attributes:
-    id (int): The ID of the library entry (read-only).
-    name (str): The name of the library entry (required).
-    location (str): The location of the library entry (required).
-    created_at (datetime): The timestamp of when the library entry was created (read-only).
-    updated_at (datetime): The timestamp of when the library entry was last updated (read-only).
-"""
 
 
 class LibraryEntryCreateSchema(Schema):
-    id = fields.Int(dump_only=True)
-    name = fields.Str(required=True)
-    location = fields.Str(required=True)
-    created_at = fields.DateTime(dump_only=True)
-    updated_at = fields.DateTime(dump_only=True)
+    # Create -> Data
+    # LibraryEntryIn
+    title: str
+    author: str
+    isbn: str
+
+
+class LibraryEntryDetailCreateSchema(Schema):
+    # Get -> Data
+    # LibraryEntryDetailOut
+    title: str
+    author: str
+    pages: int
+    rating: int
+    review: str
+    isbn: str
+    completed: bool
+    timestamp: datetime
